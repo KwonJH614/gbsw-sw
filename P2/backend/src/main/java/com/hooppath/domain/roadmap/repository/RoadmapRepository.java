3cd0c84c54d2769a3038a6f45e0814b6045e52d3
KwonJH614 <madogkwon@gmail.com>
2026-05-29 09:01:20 +0900

    chore: make deploy branch P2 only

--- P2/backend/src/main/java/com/hooppath/domain/roadmap/repository/RoadmapRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hooppath.domain.roadmap.repository;
-
-import com.hooppath.domain.roadmap.entity.Roadmap;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
-}
